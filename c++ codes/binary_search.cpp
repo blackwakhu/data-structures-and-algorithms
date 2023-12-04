fc14c153714a878e8e1dc93d49ef4a20caf501f5
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2023-12-04 11:25:55 +0300

    added the binary search function

--- c++ codes/binary_search.cpp
+++ c++ codes/binary_search.cpp
@@ -1,11 +1,27 @@
 #include<iostream>
 #include<vector>
 
+
 void printArray(vector<int> arr){
   for(int a: arr)
     cout<<a<<endl;
   cout<<endl;
 }
+
+
+int binarySearch(vector<int> arr, int item, int size, int start){
+  while(start <= size){
+    int mid = start + (size - start)/2;
+    if(arr[mid] == item)
+      return mid;
+    else if(arr[mid]<item)
+      start = mid + 1;
+    else
+      size = mid - 1;
+  }
+  return -1;
+}
+
 
 int main(){
   vector<int> vec = {1,2,3,4,5,6,7,8,9};
