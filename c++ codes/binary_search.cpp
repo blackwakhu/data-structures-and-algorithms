814c8d2c573c6b4cf8006aad8e8f00ade8fd28d5
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2023-12-04 11:13:19 +0300

    added a print array function

--- c++ codes/binary_search.cpp
+++ c++ codes/binary_search.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 #include<vector>
 
+void printArray(vector<int> arr){
+  for(int a: arr)
+    cout<<a<<endl;
+  cout<<endl;
+}
+
 int main(){
   vector<int> vec = {1,2,3,4,5,6,7,8,9};
+  printArray(vec);
   return 0;
 }
