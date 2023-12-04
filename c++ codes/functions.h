674510572eafedb07d5f7ed1cc9f01f361d436e2
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2023-12-04 11:48:46 +0300

    added merge sort

--- c++ codes/functions.h
+++ c++ codes/functions.h
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 
+
 using namespace std;
 
 void printArray(vector<int> arr){
@@ -9,11 +10,13 @@ void printArray(vector<int> arr){
   cout<<endl;
 }
 
+
 void swap(int &a, int &b){
   int temp = a;
   a = b;
   b = temp;
 }
+
 
 void bubbleSort(vector<int> &arr, int size){
   for(int i = 0 ; i < size ; i++ ){
@@ -22,4 +25,49 @@ void bubbleSort(vector<int> &arr, int size){
         swap(arr[j], arr[j+1]);
     }
   }
+}
+
+
+void sort(vector<int> & bar) {
+    if (bar.size() <= 1) return;
+
+    int mid = bar.size() / 2;
+    vector<int> left;
+    vector<int> right;
+
+    for (size_t j = 0; j < mid;j++)
+        left.push_back(bar[j]);
+    for (size_t j = 0; j < (bar.size()) - mid; j++)
+        right.push_back(bar[mid + j]);
+
+    sort(left);
+    sort(right);
+    mergeSort(left, right, bar);
+}
+void mergeSort(vector<int>&left, vector<int>& right, vector<int>& bars)
+{
+    int nL = left.size();
+    int nR = right.size();
+    int i = 0, j = 0, k = 0;
+
+    while (j < nL && k < nR) 
+    {
+        if (left[j] < right[k]) {
+            bars[i] = left[j];
+            j++;
+        }
+        else {
+            bars[i] = right[k];
+            k++;
+        }
+        i++;
+    }
+    while (j < nL) {
+        bars[i] = left[j];
+        j++; i++;
+    }
+    while (k < nR) {
+        bars[i] = right[k];
+        k++; i++;
+    }
 }
